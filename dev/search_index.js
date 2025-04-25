50d84a40a456a08b71a5b7545a5bccbf85675f45
Documenter.jl <documenter@juliadocs.github.io>
2025-04-25 00:28:37 +0000

    build based on 54a2d4a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkNestedDicts","category":"page"},{"location":"#OkNestedDicts","page":"Home","title":"OkNestedDicts","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkNestedDicts.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkNestedDicts]","category":"page"}]
+}
