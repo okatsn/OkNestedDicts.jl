914ad93219a00f4943c394d3300cd755cd73a6af
okatsn <okatsn@gmail.com>
2025-06-30 09:52:49 +0800

    Fix a typo.

--- src/dict2nt.jl
+++ src/dict2nt.jl
@@ -2,6 +2,6 @@
 (Nested) Dictionary to NamedTuple.
 """
 function dict2nt(d::Dict)
-    return NamedTuple(Symbol(k) => (v isa Dict ? dict_to_namedtuple(v) : v)
+    return NamedTuple(Symbol(k) => (v isa Dict ? dict2nt(v) : v)
                       for (k, v) in pairs(d))
 end
