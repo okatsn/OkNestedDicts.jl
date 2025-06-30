bad4317e92c8c07752e08c495d75f4cc17e9397f
okatsn <okatsn@gmail.com>
2025-06-30 10:26:48 +0800

    Add Docstring

--- src/evalvalues.jl
+++ src/evalvalues.jl
@@ -1,3 +1,7 @@
+"""
+Parse and `eval` any string values in a (nested) dictionary.
+Error will be raised if the string is not able to be parsed or evaluated.
+"""
 evalvalues(obj::Dict) = Dict(k => evalvalues(v) for (k, v) in obj)
 evalvalues(obj::String) = eval(Meta.parse(obj))
 evalvalues(obj) = obj  # fallback for any other type
